96b86c045dc63fb951ad7bf570093e5c105baac0
이루 <riruyaaa@gmail.com>
2026-07-14 20:32:29 +0900

    Auto-save

--- app.js
+++ app.js
@@ -28,6 +28,8 @@ categories.forEach(cat => {
   categoryFilter.appendChild(opt);
 });
 
+const LOCAL_VIDEOS = new Set([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21]);
+
 function thumbUrl(id) {
   return `https://img.youtube.com/vi/${id}/hqdefault.jpg`;
 }
